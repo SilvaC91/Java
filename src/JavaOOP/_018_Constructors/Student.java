10bc890e89989c241cbdd725bbf1ef03c58a8157
Silvano <giorgia.impro@Giorgias-Air.homenet.telecomitalia.it>
2023-05-03 01:37:55 +0200

    Eseguito esercizio 18

--- src/JavaOOP/_018_Constructors/Student.java
+++ src/JavaOOP/_018_Constructors/Student.java
@@ -1,4 +1,31 @@
 package JavaOOP._018_Constructors;
 
 public class Student {
+
+    String name;
+    int age;
+
+    Student(String name, int age){
+        System.out.println("Constructing");
+        this.name = name;
+        this.age = age;
+        System.out.println("Constructed");
+
+    }
+    public void printStudent(){
+        System.out.println("Student{" +
+                "\nname = " + name +
+                "\nage = " + age + "\n" +
+                "}");
+    }
+    Student(String name) {
+        this.name = name;
+    }
+    Student(int age) {
+        this.age = age;
+    }
+    Student(){
+
+    }
+
 }
