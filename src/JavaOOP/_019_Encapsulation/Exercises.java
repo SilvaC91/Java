0a6d08198afa1eebad28be4ddc69c5f18eb2d90d
Silvano <giorgia.impro@Giorgias-Air.homenet.telecomitalia.it>
2023-05-03 12:12:11 +0200

    Eseguito esercizio 19

--- src/JavaOOP/_019_Encapsulation/Exercises.java
+++ src/JavaOOP/_019_Encapsulation/Exercises.java
@@ -1,7 +1,62 @@
 package JavaOOP._019_Encapsulation;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Exercises {
     public static void main(String[] args) {
+        exercise1();
+        exercise2();
+    }
 
+    /**
+     * 1: Copy over the student class
+     *
+     *    Make sure there are 5 fields inside, if they are not simply create some more.
+     *    make every field private first.
+     *
+     *    Then create getters and setters for each of these 5 fields.
+     *
+     *    Once ready use all 5 below
+     */
+    private static void exercise1() {
+        System.out.println("Exercise 1:");
+
+        // Write and use your 5 getters and setters!
+        Student student1 = new Student("Mario","Rossi",19,1,"Economia");
+
+        student1.setName("Roberto");
+        student1.setLastName("Casale");
+        student1.setAge(23);
+        student1.setYearOfCourse(3);
+        student1.setCourse("Informatica");
+
+        System.out.println("Name = " + student1.getName()
+        + "\nLast name = " + student1.getLastName()
+        + "\nAge = " + student1.getAge()
+        + "\nYear of course = " + student1.getYearOfCourse()
+        + "\nCourse = " + student1.getCourse());
+    }
+
+    /**
+     * 2: Create 3 setter constraints for the student class
+     *
+     *    Think about what reasonably constraints are?
+     *
+     *    Should a student be able to be a negative age?
+     *
+     *    Create a counter for one of the getters, try to think about
+     *    what other code could be inside a getter? Why not write it?
+     */
+    private static void exercise2() {
+        System.out.println("\nExercise 2:");
+
+        //Write your code here
+        Student std = new Student("Sonia", "Reis", 25, 5, "Lettere");
+
+        System.out.println(std.getName());
+        std.setName("Giulia");
+        System.out.println(std.getName());
+        System.out.println(std.getNameCounter());
     }
 }
--- src/JavaOOP/_019_Encapsulation/Student.java
+++ src/JavaOOP/_019_Encapsulation/Student.java
@@ -1,4 +1,79 @@
 package JavaOOP._019_Encapsulation;
 
 public class Student {
+
+    private String name;
+    private String lastName;
+    private int age;
+    private int yearOfCourse;
+    private String course;
+    private int nameCounter = 0;
+
+
+    Student(String name, String lastName, int age, int yearOfCourse, String course){
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+        this.yearOfCourse = yearOfCourse;
+        this.course = course;
+    }
+    String getName(){
+        nameCounter++;
+        return this.name;
+    }
+    String getLastName(){
+        return this.lastName;
+    }
+    int getAge(){
+        return this.age;
+    }
+    int getYearOfCourse(){
+        return this.yearOfCourse;
+    }
+    String getCourse(){
+        return this.course;
+    }
+    void setName(String name){
+        if(name.matches(".*\\d.*")){
+            System.out.println("Invalid name");
+        }else {
+            this.name = name;
+        }
+    }
+    void setLastName(String lastName){
+        if(lastName.matches(".*\\d.*")){
+            System.out.println("Invalid last name");
+        }else {
+            this.lastName = lastName;
+        }
+    }
+    void setAge(int age){
+        if(age < 0){
+            System.out.println("Invalid value of age");
+        }else{
+            this.age = age;
+        }
+    }
+    void setYearOfCourse(int yearOfCourse){
+        if(yearOfCourse < 1 || yearOfCourse > 5){
+            System.out.println("Invalid value of year of course");
+        }else{
+            this.yearOfCourse = yearOfCourse;
+        }
+    }
+
+    void setCourse(String course){
+        this.course = course;
+    }
+    public void printStudent(){
+        System.out.println("Student{" +
+                "\nname = " + name +
+                "\nage = " + age + "\n" +
+                "}");
+    }
+    int getNameCounter(){
+        return this.nameCounter;
+    }
+
+
 }
